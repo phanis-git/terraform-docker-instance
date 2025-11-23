37030934f8f777d2169f08e53a43a411d0584a29
phanis-git <phanipspkmec@gmail.com>
2025-11-23 22:47:14 +0530

    added sleep 20

--- user-data.sh
+++ user-data.sh
@@ -5,7 +5,7 @@ growpart /dev/nvme0n1 4
 pvresize /dev/nvme0n1p4
 lvextend -L +30G /dev/mapper/RootVG-varVol 
 xfs_growfs /var
-
+sleep 20 &
 sudo dnf -y install dnf-plugins-core
 sudo dnf config-manager --add-repo https://download.docker.com/linux/rhel/docker-ce.repo
 sudo dnf -y install docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
